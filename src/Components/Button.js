5018165fd94d12ed68d751da8b7dcc01c045f921
nicogulo <nicogulobelas@gmail.com>
2021-09-15 21:41:49 +0700

    filter

---
+++ src/Components/Button.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+function Button({ type, id, handleClick }) {
+  // const handleClick = (cat) => {
+  //   filter(cat);
+  // };
+
+  // () => filter(cat)
+  return (
+    <div className="buttons">
+      <button
+        id={type}
+        type={type}
+        // onClick={(e) => e.preventDefault(filter(button))}
+        onClick={handleClick}
+        className="btn"
+        key={id}
+      >
+        {type}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
+
+// import React, { useState } from 'react';
+
+// function Button({ button, filter }) {
+//   const [clickedId, setClickedId] = useState(-1);
+
+//   const handleClick = (event, i) => {
+//     console.log(i);
+//     setClickedId(i);
+//     // doSomethingAfterClick(event);
+//   };
+//   return (
+//     <div className="">
+//       {button.map((cat, i) => (
+//         <button
+//           type="button"
+//           name={cat._id}
+//           onClick={(event) => {
+//             handleClick(event, i);
+//             filter(cat);
+//           }}
+//           className={`py-4 pl-3 customButton ${
+//             i === clickedId ? 'active' : ''
+//           }`}
+//         >
+//           {cat}
+//         </button>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Button;
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// function Button({ button, filter }) {
+//   const [apps, setApps] = useState([]);
+
+//   const url = 'https://data-polahub.herokuapp.com/api/data-app';
+
+//   useEffect(() => {
+//     getIconApp();
+//   }, []);
+
+//   const getIconApp = () => {
+//     axios
+//       .get(`${url}`)
+//       .then((res) => {
+//         // handle success
+//         const allIconApp = res.data;
+//         // console.log(allIconApp);
+//         setApps(allIconApp);
+//       })
+//       .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//       });
+//   };
+
+//   return (
+//     <div className="buttons">
+//       {apps.map((cat, i) => {
+//         return (
+//           <button type="button" onClick={() => filter(cat)} className="btn">
+//             {cat.type}
+//           </button>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default Button;
---
+++ src/Components/Menu.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+function Menu({ image, name, type, id }) {
+  return (
+    <div className="item">
+      <div className="item-con" key={id}>
+        <div className="item-container">
+          <img src={image} alt={name} />
+          <h2>{name}</h2>
+          <p>{type}</p>
+          <p>{id}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
+
+// import React from 'react';
+
+// function Menu({ menuItem }) {
+//   return (
+//     <div className="item">
+//       {menuItem.map((app, index2) => {
+//         return (
+//           <div className="item-con" key={app._id}>
+//             <div className="item-container">
+//               <img src={app.imageUrl} alt="" />
+//               <h2>{app.name}</h2>
+//               <p>{app._id}</p>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default Menu;
+
+// import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+
+// function Menu({ menuItem }) {
+//   const [apps, setApps] = useState([]);
+
+//   const url = 'https://data-polahub.herokuapp.com/api/data-app';
+
+//   useEffect(() => {
+//     getIconApp();
+//   }, []);
+
+//   const getIconApp = () => {
+//     axios
+//       .get(`${url}`)
+//       .then((res) => {
+//         // handle success
+//         const allIconApp = res.data;
+//         // console.log(allIconApp);
+//         setApps(allIconApp);
+//       })
+//       .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//       });
+//   };
+
+//   return (
+//     <div className="item">
+//       {apps.map((app, index2) => {
+//         return (
+//           <div className="item-con" key={app._id}>
+//             <div className="item-container">
+//               <img src={app.imageUrl} alt="" />
+//               <h2>{app.name}</h2>
+//               <p>{app._id}</p>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default Menu;
